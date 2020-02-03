a7614f52a48f4d7c95ef1e5137fd9b8d97b44409
Andrew Weiss <wandrew8@gmail.com>
2020-02-03 10:12:25 -0800

    featured items added to home component

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,7 +1,37 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
+
+
+function RenderItems({item}) {
+    if (item) {
+        return (
+            <Card
+                featuredTitle={item.name}
+                image={require('./images/react-lake.jpg')}
+            >
+                <Text
+                    style={{margin:10}}>
+                        {item.description}
+                </Text>
+            </Card>
+        )
+    }
+    return <View />
+}
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            campsites: CAMPSITES,
+            partners: PARTNERS,
+            promotions: PROMOTIONS,
+        };
+    }
 
     static navigationOptions = {
         title: 'Home'
@@ -9,9 +39,14 @@ class Home extends React.Component {
 
     render() {
         return (
-            <View>
-                <Text>Home Component</Text>
-            </View>
+            <ScrollView>
+                <RenderItems
+                    item={this.state.campsites.filter(campsite => campsite.featured)[0]} />
+                <RenderItems
+                    item={this.state.promotions.filter(promotion => promotion.featured)[0]} />
+                <RenderItems
+                    item={this.state.partners.filter(partner => partner.featured)[0]} />    
+            </ScrollView>
         )
     }
 }
