6b46fd9b36775f2a29653ea26bfed315e4c4d771
Andrew Weiss <wandrew8@gmail.com>
2020-02-17 09:09:20 -0800

    favorties component added

--- nucampsite/components/MainComponent.js
+++ nucampsite/components/MainComponent.js
@@ -5,6 +5,7 @@ import About from './AboutComponent';
 import Contact from './ContactComponent'
 import CampsiteInfo from './CampsiteInfoComponent';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoritesComponent';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { createStackNavigator, createDrawerNavigator, DrawerItems } from 'react-navigation';
@@ -138,6 +139,29 @@ const ReservationNavigator = createStackNavigator (
     }
 )
 
+const FavoritesNavigator = createStackNavigator (
+    {
+        Favorites: { screen: Favorites },
+    },
+    {
+        navigationOptions: ({ navigation }) => ({
+            headerStyle: {
+                backgroundColor: '#34ace0'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                    name='heart'
+                    type='font-awesome'
+                    iconStyle={styles.stackIcon}
+                    onPress={() => navigation.toggleDrawer()}
+                    />
+        })
+    }
+)
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView
@@ -191,6 +215,20 @@ const MainNavigator = createDrawerNavigator(
                 drawerIcon: ({tintColor}) => (
                     <Icon
                         name='tree'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }  
+        },
+        Favorites: { 
+            screen: FavoritesNavigator,
+            navigationOptions: {
+                drawerLabel: 'My Favorites',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='heart'
                         type='font-awesome'
                         size={24}
                         color={tintColor}
--- nucampsite/shared/baseURL.js
+++ nucampsite/shared/baseURL.js
@@ -1,8 +1,8 @@
-// Home
-// export const baseUrl = 'http://192.168.1.181:3001/';
 // The Den
+export const baseUrl = 'http://192.168.1.91:3001/';
+// Home
 // export const baseUrl = 'http://10.0.0.7:3001/';
 // Woods Cafe
 // export const baseUrl = 'http://192.168.223.3:3001/';
 // Bellevue WeWork
-export const baseUrl = 'http://10.167.71.157:3001/';
+// export const baseUrl = 'http://10.167.71.157:3001/';
